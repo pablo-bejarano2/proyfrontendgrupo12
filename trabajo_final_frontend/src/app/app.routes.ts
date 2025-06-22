90880f8407f84b92390953eafe29596106dcccd5
pablo-bejarano2 <pablobejarano2112@gmail.com>
2025-06-22 15:27:03 -0300

    add contact page

--- trabajo_final_frontend/src/app/app.routes.ts
+++ trabajo_final_frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductDetailComponent } from './components/public/product-detail/product-detail';
+import { ContactComponent } from './components/public/contact/contact';
 
 export const routes: Routes = [
-  { path: 'product', component: ProductDetailComponent }
+  { path: 'product', component: ProductDetailComponent },
+  { path: 'contact', component: ContactComponent },
 ];

9ca54ff0aec19bb3d2dd4e7c4f3fedc928ac0165
marcosAchambi <44516075@fi.unju.edu.ar>
2025-06-23 00:41:31 -0300

    Add route for `ProductList` component to app routing module

--- trabajo_final_frontend/src/app/app.routes.ts
+++ trabajo_final_frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductDetailComponent } from './components/public/product-detail/product-detail';
 import { ContactComponent } from './components/public/contact/contact';
 import { Home } from './components/public/home/home';
+import { ProductList} from './components/public/product-list/product-list';
 
 export const routes: Routes = [
   {path: '', component: Home},
   { path: 'product', component: ProductDetailComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'product-list', component:  ProductList},
 ];

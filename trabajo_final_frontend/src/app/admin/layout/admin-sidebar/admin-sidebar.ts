125d4cb62b2d66f936e5f53c740249ca65a6c322
Ignacio Martín <fibias2004@gmail.com>
2025-06-25 01:25:44 -0300

    Añado ejemplos de componentes para Admin

--- trabajo_final_frontend/src/app/admin/layout/admin-sidebar/admin-sidebar.ts
+++ trabajo_final_frontend/src/app/admin/layout/admin-sidebar/admin-sidebar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [ RouterLink],
+  imports: [ RouterLink, RouterLinkActive],
   templateUrl: './admin-sidebar.html',
   styleUrl: './admin-sidebar.css'
 })

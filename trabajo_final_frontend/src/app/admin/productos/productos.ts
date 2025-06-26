ba5ddf09a09ba15f52f38f3bcda7dd408db10233
Ignacio Martín <fibias2004@gmail.com>
2025-06-26 03:49:15 -0300

    Revert "Conexión Backend Productos con Admin - Productos Template"
    This reverts commit 3e118dcb4896f5eca90599f0ce79127ded9015a5.

--- trabajo_final_frontend/src/app/admin/productos/productos.ts
+++ trabajo_final_frontend/src/app/admin/productos/productos.ts
@@ -1,75 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductoService, Producto } from '../../services/producto';
-
-
 @Component({
   selector: 'app-productos',
   imports: [CommonModule],
   templateUrl: './productos.html',
   styleUrl: './productos.css'
 })
-
-export class Productos implements OnInit {
-
-  productos: Producto[] = [];
-
-  constructor(private productoService: ProductoService){}
-
-  cargarProductos(): void {
-    this.productoService.obtenerProductos().subscribe(
-      (productos: Producto[]) => {
-        this.productos = productos;
-      },
-      (error) => {
-        console.error('Error al cargar productos:', error);
-      }
-    );
-  }
-
-  //Método para Crear un nuevo Producto
-  crearProducto(nuevoProducto: Partial<Producto>): void {
-    this.productoService.crearProducto(nuevoProducto).subscribe(
-      (productoCreado: Producto) => {
-        this.productos.push(productoCreado);
-        console.log('Producto creado:', productoCreado);
-      },
-      (error) => {
-        console.error('Error al crear producto:', error);
-      }
-    );
-  }
-  // Método para eliminar un producto por ID
-  eliminarProducto(id: string): void {
-    this.productoService.eliminarProducto(id).subscribe(
-      () => {
-        this.productos = this.productos.filter(producto => producto._id !== id);
-        console.log('Producto eliminado:', id);
-      },
-      (error) => {
-        console.error('Error al eliminar producto:', error);
-      }
-    );
-  }
-  // Método para actualizar un producto
-  actualizarProducto(id: string, productoActualizado: Partial<Producto>): void {
-    this.productoService.actualizarProducto(id, productoActualizado).subscribe(
-      (producto: Producto) => {
-        const index = this.productos.findIndex(p => p._id === id);
-        if (index !== -1) {
-          this.productos[index] = producto;
-        }
-        console.log('Producto actualizado:', producto);
-      },
-      (error) => {
-        console.error('Error al actualizar producto:', error);
-      }
-    );
-  }
-
-
-  ngOnInit(): void {
-    this.cargarProductos();
-  }
+export class Productos {
 
 }

a96eaa9a8383cf4f9b4b5a8344084025eaeb42f8
pablo-bejarano2 <pablobejarano2112@gmail.com>
2025-07-08 05:27:40 -0300

    documentacion de pedido y contacto

--- trabajo_final_frontend/src/app/services/pedido.ts
+++ trabajo_final_frontend/src/app/services/pedido.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@/environments/environment';
 import { map, Observable } from 'rxjs';
-
+// Define Pedido
 export interface Pedido {
   _id: string;
   cliente?: { _id: string; nombres: string };
@@ -31,31 +31,32 @@ export class PedidoService {
     return this.http.post<{ status: string, msg: string, pedido: Pedido }>(this.API_URL, pedido)
       .pipe(map(res => res.pedido));
   }
-
+//Obtiene los pedidos
+  // No transforma los campos de cliente, direccion, etc. a IDs
   getPedidos(): Observable<Pedido[]> {
     return this.http.get<{ pedidos: Pedido[] }>(this.API_URL).pipe(
       map(res => res.pedidos)
     );
   }
-
+// Obtiene un pedido por ID
   obtenerPedidoPorId(id: string): Observable<Pedido> {
     return this.http.get<{ pedido: Pedido }>(`${this.API_URL}/${id}`).pipe(
       map(res => res.pedido)
     );
   }
-
+//Obtiene pedidos por cliente ID
   obtenerPedidoPorClienteId(clienteId: string): Observable<Pedido[]> {
     return this.http.get<{ pedidos: Pedido[] }>(`${this.API_URL}/cliente/${clienteId}`).pipe(
       map(res => res.pedidos)
     );
   }
-
+// Actualiza un pedido
   updatePedido(id: string, pedido: Partial<Pedido>): Observable<Pedido> {
     // Envía el objeto tal cual, sin transformar campos a IDs
     return this.http.put<{ status: string, msg: string, pedido: Pedido }>(`${this.API_URL}/${id}`, pedido)
       .pipe(map(res => res.pedido));
   }
-
+//Elimina un pedido
   deletePedido(id: string): Observable<any> {
     return this.http.delete(`${this.API_URL}/${id}`);
   }
